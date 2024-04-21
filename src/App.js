736b06ad5717eb03ec2f7c813982576b6a1ef293
Ana_Z <113620777+Zana2234@users.noreply.github.com>
2024-04-21 10:29:25 +0200

    change links

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ function App() {
       Heloo!
       <footer>
         This project was coded by
-        <a href="https://github.com/Zana2234" target="_blank">
+        <a href="https://github.com/Zana2234" target="_blank" rel="noreferrer">
           Ana Zabaleta
         </a>
         , is
         <a
           href="https://github.com/Zana2234/weatherapp-react.git"
           target="_blank"
+          rel="noreferrer"
         >
           on GitHub
         </a>
@@ -20,6 +21,7 @@ function App() {
         <a
           href="https://master--weather-app-reactana.netlify.app/"
           target="_blank"
+          rel="noreferrer"
         >
           hosted on Netlify
         </a>
