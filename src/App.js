820ad66e0d31821eeb0665e341d20750e822ded7
Ana_Z <113620777+Zana2234@users.noreply.github.com>
2024-04-21 10:27:23 +0200

    added github links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Heloo!</div>;
+  return (
+    <div className="App">
+      Heloo!
+      <footer>
+        This project was coded by
+        <a href="https://github.com/Zana2234" target="_blank">
+          Ana Zabaleta
+        </a>
+        , is
+        <a
+          href="https://github.com/Zana2234/weatherapp-react.git"
+          target="_blank"
+        >
+          on GitHub
+        </a>
+        and
+        <a
+          href="https://master--weather-app-reactana.netlify.app/"
+          target="_blank"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
